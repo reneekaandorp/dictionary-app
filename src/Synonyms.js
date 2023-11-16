dcdaa16f98c1b30d3b3d5a7e2550d8f7af8cc19a
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2023-11-16 09:37:02 +0100

    Created Synonyms component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  return "hello from Synonyms";
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        <strong>Synonyms: </strong>
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
