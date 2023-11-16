ffdcd160dc8cfcf9146623570083477969fca6f9
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2023-11-16 11:47:52 +0100

    Created Example component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import Example from "./Example";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+
       <strong>Definition: </strong>
       {props.meaning.definition}
-      <br />
-      <strong>Example: </strong>
-      {props.meaning.example}
-      <br />
+
+      <Example example={props.meaning.example} />
+
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
